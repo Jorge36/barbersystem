cef7a421bcf40d9e0cf3c8b3aa02b43263ea57f5
Jorge36 <36677503+Jorge36@users.noreply.github.com>
2024-04-30 20:44:20 +0100

    Add files via upload

---
+++ BarberBookingSystem/src/Main.java
@@ -0,0 +1,9 @@
+import Controller.LoginController;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        // TODO code application logic here
+        new LoginController();
+    }
+}
