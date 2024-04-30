ddc7a20c1df6072206231f4f5c0d1d3e1cd3df9c
Jorge36 <36677503+Jorge36@users.noreply.github.com>
2024-04-30 20:42:22 +0100

    Delete BarberBookingSystem directory

--- BarberBookingSystem/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Controller.LoginController;
-
-public class Main {
-
-    public static void main(String[] args)  {
-        // TODO code application logic here
-        new LoginController();
-    }
-}
